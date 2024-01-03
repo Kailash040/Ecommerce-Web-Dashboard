0c870de51182749dc00ad4cd2303bc9a82bb068c
kailash <kailashrajut0575@gmail.com>
2024-01-03 21:07:09 +0530

    nextjs-auth-using-clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,10 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Button } from "@/components/ui/button"
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <main className="">
-    <h1>Ecommerce Dashboard</h1>
-    <Alert>
- 
-  <AlertTitle>Heads up!</AlertTitle>
-  <AlertDescription>
-    You can add components and dependencies to your app using the cli.
-  </AlertDescription>
-</Alert>
-<Button >Submit</Button>
-    </main>
+    <div className="h-screen">
+    <UserButton afterSignOutUrl="/"/>
+  </div>
   )
 }
