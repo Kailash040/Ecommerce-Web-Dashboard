f4e7de808cc4c584b00a66901c8d9681561bcae0
kailash <kailashrajut0575@gmail.com>
2024-01-08 18:00:27 +0530

    add alert

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
-
 import { UserButton } from "@clerk/nextjs";
 export default function SetupPage() {
   return (
     <div className="h-screen">
-    {/* <UserButton afterSignOutUrl="/"/> */}
-<h1>protected route page</h1>
-  </div>
-  )
+      {/* <UserButton afterSignOutUrl="/"/> */}
+      <h1>protected route page</h1>
+    </div>
+  );
 }
