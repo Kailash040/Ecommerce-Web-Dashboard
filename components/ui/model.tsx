35b17f0544be8e540baaffe53471f51cb693d070
kailash <kailashrajut0575@gmail.com>
2024-01-08 21:15:00 +0530

    dialog box done

--- components/ui/model.tsx
+++ components/ui/model.tsx
@@ -1,1 +1,41 @@
 "use client"
+
+import { DialogDescription } from "@radix-ui/react-dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./dialog";
+
+
+interface ModelProps {
+    title:string,
+    description:string,
+    isOpen :boolean,
+    onClose: () => void,
+    children?: React.ReactNode
+};
+
+export const Model: React.FC<ModelProps> =({
+    title,
+    description,
+    isOpen,
+    onClose,
+    children
+}) =>{
+    const onChange = (open :boolean) =>{
+        if(!open){
+            onClose()
+        }
+    };
+    return(
+    <Dialog open={isOpen} onOpenChange={onChange}>
+<DialogContent>
+    <DialogHeader>
+    <DialogTitle>
+        {title}
+    </DialogTitle>
+    <DialogDescription>
+        {description}
+    </DialogDescription>
+    </DialogHeader>
+</DialogContent>
+    </Dialog>
+    )
+}
